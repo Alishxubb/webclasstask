b31a8c26ad78c93613b0df5cac9c4a2db630abf0
Alishxubb <alishbaalish889@gmail.com>
2024-04-30 14:07:56 +0500

    classwork

---
+++ week8/week8.js
@@ -0,0 +1,25 @@
+$(function(){
+    //jq will execute this function after page load
+    $("#addbutton").click(handlebtnclick);
+    //$("#todos li").click(removeMe);
+    $("#todos").on("click", "li", removeMe);
+    
+    });
+    function handlebtnclick()
+    {
+       // alert("btn clicked");
+       var newtodo = $("#newtodo").val();
+       if(!newtodo){
+        $("#newtodo").addClass("error");
+        return;
+       }
+       $("#newtodo").removeClass("error");
+       //$("#newtodo").val("");
+       $("#todos").append("<td>" + newtodo + "</td>");
+       //$("#todos li").click(removeMe);
+    }
+    function removeMe(){
+        $(this).remove();
+        //$(this).fadeOut();
+    }
+   
